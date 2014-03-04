ed918d911577f3b8ffb36bb796f93721e9c0e332
Irene <icn2104@columbia.edu>
2014-03-04 12:04:33 -0500

    I've created a scripts file to implement Google Fusion Tables for Unemployment Data

---
+++ scripts.js
@@ -0,0 +1,59 @@
+/**
+ * @author Chidinma Irene Nwoye
+ */
+
+console.log("My Javascript is working"); //I'm testing my javascript file
+
+function dataUp(uEMPDATA){            //uEMPDATA is the local name for the JSON file
+	
+	console.log(uEMPDATA);        //I'm outputting the JSON file to be sure the function dataUp works on my javascript
+	
+	var gDataTable = new google.visualization.DataTable();
+	
+	//When I add columns, the 1st parameter is the data type in that column
+	// The 2nd parameter is the name of the column
+	gDataTable.addColumn('string', uEMPDATA.columns[0]);
+	gDataTable.addColumn('number', uEMPDATA.columns[1]);
+	
+	gDataTable.addRows(uEMPDATA.rows);
+	
+	
+	var ChartChoices = {                           //create options object to actually customize the look of our chart
+          title: "Unemployment Since 1980"       //This is a configuration option
+        };
+	//tells Google Visualization to create a line chart and give it to the
+	var ThisChart = new google.visualization.LineChart(document.getElementById('myChartDiv')); //I've created type of chart e.g. BarChart to LineChart etc
+	ThisChart.draw(gDataTable,ChartChoices);
+
+
+
+}
+
+
+function GoogleUp() {
+
+	console.log("I've put up Google!");
+	
+	// Instead of loading data from a static json file
+	//I'm going to load it from Google Fusion Table	
+	
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1L1rmYMaUAgK7tXaB9vt28njZA3ZAj4k8zHizBP9N&key=AIzaSyCgfAUYUUuX8vurL0WtKGYRGho11LEnsPI", dataUp, "json"); //I'm retrieving my the json 
+	}
+
+
+function docUp() {
+
+	console.log("My page is up!");
+
+	//I'm loading the google visualization library
+	google.load("visualization", "1", {
+		packages : ["corechart"],
+		callback : "GoogleUp"
+	});
+
+}
+
+
+$(document).ready(docUp);  //I've used $(document).ready to call my function, docUp
+
+
